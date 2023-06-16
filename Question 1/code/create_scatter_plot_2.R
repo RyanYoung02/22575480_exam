2e9a8c9283584345e50b4ceb646e0f85a0d36edb
RyanYoung02 <ryanryoung02@gmail.com>
2023-06-16 13:41:55 +0200

    completed q1 a, b

--- Question 1/code/create_scatter_plot_2.R
+++ Question 1/code/create_scatter_plot_2.R
@@ -1,19 +1,16 @@
-
-
-
-create_scatter_plot <- function(covid_data) {
+create_scatter_plot_2 <- function(covid_data) {
     # Aggregate the data
     aggregated_data <- covid_data %>%
         group_by(location) %>%
         slice(n())
 
     # Create the scatter plot
-    c<-ggplot(aggregated_data, aes(x = total_deaths_per_million, y = aged_65_older)) +
+    d<-ggplot(aggregated_data, aes(x = total_deaths_per_million, y = aged_65_older)) +
         geom_point(size = 1) +
         geom_smooth(method = "lm", se = FALSE) +
         labs(x = "COVID Deaths per Million", y = "Percent of population over 65") +
         ggtitle("COVID Deaths by Percent of population over 65") +
         theme_minimal()
-    c
+d
 
 }
